b47840f2968b1a8bc2bbfed12bd81300b2669c8f
Ana <anuleavrabie@gmail.com>
2022-09-22 17:16:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+const category = "toys";
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ana";
+
+alert(`Salut,${user}`);
+
+console.log("arr" + "- obj");
+console.log(4 + 5);
+let incr = 10,
+  decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 != 6);
+
+const isChecked = false,
+      isClose = false;
+console.log(isChecked || !isClose);
+
+
